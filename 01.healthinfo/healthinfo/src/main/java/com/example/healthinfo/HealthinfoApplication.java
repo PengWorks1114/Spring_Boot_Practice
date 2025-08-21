410cf72c89a1b41f4d219c3445726bb0db6fbd7d
peng0o0 <tomato.202504.penghc@gmail.com>
2025-08-21 11:28:24 +0900

    first commit

---
+++ 01.healthinfo/healthinfo/src/main/java/com/example/healthinfo/HealthinfoApplication.java
@@ -0,0 +1,13 @@
+package com.example.healthinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthinfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthinfoApplication.class, args);
+	}
+
+}
