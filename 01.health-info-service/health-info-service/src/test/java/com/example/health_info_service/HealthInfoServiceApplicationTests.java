ce066343f461ec4a1567d87cd44b5c6b571a53d6
peng0o0 <tomato.202504.penghc@gmail.com>
2025-08-21 11:35:07 +0900

    pj01 init

---
+++ 01.health-info-service/health-info-service/src/test/java/com/example/health_info_service/HealthInfoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.health_info_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthInfoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
