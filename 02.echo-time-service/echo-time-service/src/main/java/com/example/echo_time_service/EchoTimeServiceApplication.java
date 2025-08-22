783cb2ec94dc7abd33f78654099fc5ae7c69cd56
peng0o0 <tomato.202504.penghc@gmail.com>
2025-08-22 14:11:55 +0900

    day2 iniy

---
+++ 02.echo-time-service/echo-time-service/src/main/java/com/example/echo_time_service/EchoTimeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.echo_time_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EchoTimeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EchoTimeServiceApplication.class, args);
+	}
+
+}
---
+++ 02.echo-time-service/echo-time-service/src/test/java/com/example/echo_time_service/EchoTimeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.echo_time_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EchoTimeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
