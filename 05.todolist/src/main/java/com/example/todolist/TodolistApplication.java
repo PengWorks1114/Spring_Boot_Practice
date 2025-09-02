77b075b9d344f437513b0e020e93ba7bfcf68663
peng0o0 <tomato.202504.penghc@gmail.com>
2025-09-02 15:06:13 +0900

    backend finish

---
+++ 05.todolist/src/main/java/com/example/todolist/TodolistApplication.java
@@ -0,0 +1,11 @@
+package com.example.todolist; // 根套件
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolistApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TodolistApplication.class, args);
+    }
+}
